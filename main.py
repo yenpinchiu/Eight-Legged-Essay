2ea74e968aa783c19c916c5e80775e3e7ff05cb2
yenpinchiu <you@example.com>
2017-10-13 22:30:02 +0800

    start remove Nth node from end of list

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import Two_Sum
-import Three_Sum
-import Four_Sum
+import Remove_Nth_Node_From_End_of_List
 
 if __name__ == '__main__':
-    #sol_2Sum = Two_Sum.Solution()
-    #sol_3Sum = Three_Sum.Solution()
-    sol_4Sum = Four_Sum.Solution()
-    #print(sol_3Sum.threeSum2([-1, 0, 1, 2, -1, -4]))
-    #print(sol_3Sum.threeSum2([0, 0, 0]))
-    #print(sol_3Sum.threeSum2([0, 0]))
-    #print(sol_3Sum.threeSum2([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]))
-    #print(sol_3Sum.threeSum2([-1,0,1,2,-1,-4]))
-    #print(sol_3Sum.threeSum2([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
-    #print(sol_2Sum.twoSum([2, 7, 11, 15], 9))
-    #print(sol_2Sum.twoSum2([2, 7, 11, 15], 9))
-    #print(sol_2Sum.twoSum2([3, 2, 4], 6))
-    #print(sol_2Sum.twoSum2([3, 3], 6))
-    print(sol_4Sum.fourSum([1, 0, -1, 0, -2, 2], 0))
-    print(sol_4Sum.fourSum([0, 0, 0, 0], 0))
+    s = Remove_Nth_Node_From_End_of_List.Solution()
+
