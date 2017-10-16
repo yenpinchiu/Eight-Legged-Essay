faa2e6853a64bb9ac6aa87eb9ef66420cfdf2779
yenpinchiu <yenpinchiu@bridgewell.com>
2017-10-16 15:57:29 +0800

    add codility square whole

---
+++ Codility_Number_Of_Square.py
@@ -0,0 +1,12 @@
+import math
+
+class Solution(object):
+    def FindSquareNumNum(self, a, b):
+        min_squre_num = math.ceil(math.sqrt(a))
+        max_squre_num = math.floor(math.sqrt(b))
+
+        return max_squre_num - min_squre_num + 1
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.FindSquareNumNum(4, 17))
