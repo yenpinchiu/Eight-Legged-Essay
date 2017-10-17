de2093787e394c8664c246856efa822a98c815ab
yenpinchiu <you@example.com>
2017-10-18 04:21:52 +0800

    complete sort with single swap O(n) solution

--- Codility_Single_Swap_Sort.py
+++ Codility_Single_Swap_Sort.py
@@ -13,18 +13,39 @@ class Solution(object):
         return True
 
     def isSingleSwapSorted2(self, nums):
+        already_sort = False
+
         for i in range(0, len(nums)):
             if i + 1 < len(nums) and nums[i + 1] < nums[i]:
-                for j in range(i + 1, len(nums)):
+                if already_sort is True:
+                    return False
+
+                for j in range(len(nums) - 1, i, -1):
                     if nums[i] > nums[j]:
                         tmp = nums[i]
                         nums[i] = nums[j]
                         nums[j] = tmp
+                        already_sort = True
+                
+                if already_sort is False:
+                    return False
 
-        return nums
+        return True
 
 if __name__ == "__main__":
     s = Solution()
     print(s.isSingleSwapSorted([1, 5, 3, 3, 7]))
+    print(s.isSingleSwapSorted([1, 5, 3, 7]))
     print(s.isSingleSwapSorted([1, 9, 3, 4, 7, 2]))
     print(s.isSingleSwapSorted([1, 9, 3, 5, 4, 7, 2]))
+    print(s.isSingleSwapSorted([]))
+    print(s.isSingleSwapSorted([1, 1, 1, 1, 0]))
+    print(s.isSingleSwapSorted([1, 6, 5, 2]))
+    print("---------------------------")
+    print(s.isSingleSwapSorted2([1, 5, 3, 3, 7]))
+    print(s.isSingleSwapSorted2([1, 5, 3, 7]))
+    print(s.isSingleSwapSorted2([1, 9, 3, 4, 7, 2]))
+    print(s.isSingleSwapSorted2([1, 9, 3, 5, 4, 7, 2]))
+    print(s.isSingleSwapSorted2([]))
+    print(s.isSingleSwapSorted2([1, 1, 1, 1, 0]))
+    print(s.isSingleSwapSorted2([1, 6, 5, 2]))
