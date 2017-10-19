017409cd0ffe054c53df861c73156663cf112404
yenpinchiu <you@example.com>
2017-10-20 06:54:32 +0800

    first version of codility clocks, seems timeout

--- Codility_Clocks.py
+++ Codility_Clocks.py
@@ -1,25 +1,37 @@
-class Solution(object):
-    def numOfPairs(self, clocks, n, m, p):
-        for clock in clocks:
-            handIntervalList = self.handIntervalList(clock)
-            print(handIntervalList)
+def solution(A, P):
+    clock_dict = {}
+    pair_list = []
 
+    if A is None:
         return 0
-    
-    def handIntervalList(self, clock):
-        intervalList = []
-        for hand_index in range(0, len(clock)):
-            if hand_index + 1 >= len(clock):
-                next_hand_index = 0
-            else:
-                next_hand_index = hand_index + 1
 
-            intervalList.append(clock[next_hand_index] - clock[hand_index])
+    for ci in xrange(0, len(A)):
+        handIntervals = handIntervalList(sorted(A[ci]), P)
+        if str(handIntervals) in clock_dict:
+            for ci2 in clock_dict[str(handIntervals)]:
+                pair_list.append([ci2, ci])
         
-        return intervalList
+        for ri in xrange(0, len(A[0])):
+            handIntervalsRotated = handIntervals[ri:len(handIntervals)]
+            handIntervalsRotated.extend(handIntervals[0:ri])
+
+            if str(handIntervalsRotated) not in clock_dict:
+                clock_dict.update({str(handIntervalsRotated):set()})
+
+            clock_dict[str(handIntervalsRotated)].add(ci)
+
+    return len(pair_list)
+            
+def handIntervalList(Aci, P):
+    intervalList = []
+    for hi in xrange(0, len(Aci)):
+        if hi + 1 >= len(Aci):
+            intervalList.append(P - Aci[hi] + Aci[0] )
+        else:
+            intervalList.append(Aci[hi + 1] - Aci[hi])
+    
+    return intervalList
 
 if __name__ == '__main__':
     clocks = [[1, 2], [2, 4], [4, 3], [2, 3], [1, 3]]
-
-    s = Solution()
-    print(s.numOfPairs(clocks, 5, 2, 4))
+    print(solution(clocks, 4))
